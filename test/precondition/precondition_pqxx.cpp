2af9ef40b652128fdc8fd054c5650d2f88360951
AndrewRademacher <andrewrademacher@icloud.com>
2019-10-05 23:12:11 -0500

    Add default connection string collection to precondition testing.

--- test/precondition/precondition_pqxx.cpp
+++ test/precondition/precondition_pqxx.cpp
@@ -1,6 +1,7 @@
 #include <catch2/catch.hpp>
 
 #include <pqxx/pqxx>
+#include <spdlog/spdlog.h>
 
 // CREATE TABLE points(
 //    id bigserial not null,
@@ -11,8 +12,20 @@
 //    PRIMARY KEY (id)
 //);
 
+const char* default_connection_string() {
+    static const char* default_str = "host=localhost port=5432";
+    char*              provided_str;
+    provided_str = getenv("ASYNC_PQXX_TEST_DATABASE");
+
+    spdlog::info("DATABASE CONNECTION STRING: {}", provided_str ? provided_str : default_str);
+    if (provided_str)
+        return provided_str;
+    else
+        return default_str;
+}
+
 TEST_CASE("precondition_pqxx: select 1 from database", "[precondition_pqxx]") {
-    pqxx::connection connection("host=localhost port=5432");
+    pqxx::connection connection(default_connection_string());
     pqxx::work       work(connection);
 
     auto r = work.exec1("SELECT 1;");
