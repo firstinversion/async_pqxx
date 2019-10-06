f538f4246a15e02c6086552e5ecbb4ac7a3c85be
AndrewRademacher <andrewrademacher@icloud.com>
2019-10-05 23:17:53 -0500

    Updated conan test.

--- conan_test/main.cpp
+++ conan_test/main.cpp
@@ -7,7 +7,7 @@ int main() {
     async_pqxx::manager     manager(4, "host=localhost port=5432");
     boost::asio::io_context ioc;
     boost::asio::spawn(ioc, [&](boost::asio::yield_context yield) {
-        auto result = manager.exec1("SELECT 1;", yield);
+        auto result = manager.async_exec1("SELECT 1;", yield);
         assert(result[0].as<int>() == 1);
     });
     ioc.run();
