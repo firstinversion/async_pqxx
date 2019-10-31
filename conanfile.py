2c39665d57406233947e0fab6c29fce713dc6011
Andrew Rademacher <andrewrademacher@icloud.com>
2019-10-31 11:42:55 -0500

    Fixed image references.

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class BeastMode(ConanFile):
     name = "async_pqxx"
-    version = "0.1.5"
+    version = "0.1.6"
     license = "MIT"
     author = ("Andrew Rademacher <andrewrademacher@icloud.com>")
     url = "https://bitbucket.org/firstinversion/async_pqxx/"
